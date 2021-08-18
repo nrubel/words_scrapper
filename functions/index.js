97c6a0bac6ee3493e7a13239dc0a5c5a709a1111
Nasir <nasir.rubel99@gmail.com>
2021-08-18 15:39:32 +0600

    firebase all words

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,9 @@
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
+const functions = require("firebase-functions")
+const {info, error, log} = require("firebase-functions/lib/logger")
+const admin = require("firebase-admin")
 const axios = require('axios')
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer')
+const {omit} = require('lodash')
 
 admin.initializeApp();
 
@@ -10,25 +12,26 @@ const runtimeOpts = {
   memory: '512MB'
 }
 
-exports.getWordResult = functions.runWith(runtimeOpts).region(`us-central1`).https.onCall(async (data, context) => {
-  try{
+const HTTPS = functions.runWith(runtimeOpts).region(`us-central1`).https
+const baseUrl = 'https://www.wordsapi.com'
+
+exports.getWordResult = HTTPS.onCall(async (data, context) => {
+  try {
     const {word, type} = data
     const words = admin.firestore().collection('words').doc(word)
     const doc = await words.get()
-    let result;
-    if(doc.data()) {
-      console.log(`result from firestore`)
+    let result, when, encrypted
+    const url = w => `${baseUrl}/mashape/words/${w}?when=${when}&encrypted=${encrypted}`
+    if (doc.data()) {
+      info(`result from firestore: ${word}`)
       result = doc.data()
-    }
-    else{
-      console.log(`result from wordsapi`, word)
+    } else {
+      info(`result from wordsapi: ${word}`)
       const browser = await puppeteer.launch()
       const page = await browser.newPage()
-      const baseUrl = 'https://www.wordsapi.com'
 
       await page.goto(baseUrl, {waitUntil: "load"})
 
-      let when, encrypted
       const scripts = await page.$$eval('script', els => els.map((e) => e.textContent))
       scripts
         .filter((e) => e !== '')[0]
@@ -41,10 +44,9 @@ exports.getWordResult = functions.runWith(runtimeOpts).region(`us-central1`).htt
           if (i === 1) when = e;
           if (i === 3) encrypted = e;
         })
-      if(when && encrypted) {
-        const url = `${baseUrl}/mashape/words/${word}?when=${when}&encrypted=${encrypted}`
-        console.log(url)
-        const res = await axios.get(url)
+      if (when && encrypted) {
+        log(`submit word url`, url(word))
+        const res = await axios.get(url(word))
         await words.set(res.data)
         result = res.data
       }
@@ -52,12 +54,39 @@ exports.getWordResult = functions.runWith(runtimeOpts).region(`us-central1`).htt
       await browser.close()
     }
 
-    return type === 'everything' ? result : {
+    log(`desired data returned for word`, word)
+    return {
       word,
-      [type]: result.results.map(r => r[type]).flat(100).filter(e => !!e),
+      current: type === 'everything' ? result : result.results.map(r => r[type]).flat(100).filter(e => !!e),
+      futureQuery: {
+        when,
+        encrypted,
+        words: result.results.map(e => Object.values(omit(e, ['definition', 'examples']))
+          .flat(2)).flat()
+          .filter((v, i, s) => s.indexOf(v) === i)
+      }
     }
-  }catch (e) {
-    console.error(e.details)
+  } catch (e) {
+    error(e.details)
+    return e.message
+  }
+})
+
+exports.loopWords = HTTPS.onCall(async (data, context) => {
+  try {
+    const {word, when, encrypted} = data
+    const words = admin.firestore().collection('words').doc(word)
+    const doc = await words.get()
+    let url = `${baseUrl}/mashape/words/${word}?when=${when}&encrypted=${encrypted}`
+    if (!doc.data()) {
+      info(`result from wordsapi: ${word}`)
+      const res = await axios.get(url)
+      await words.set(res.data)
+    }
+
+    return null
+  } catch (e) {
+    error(e.details)
     return e.message
   }
 })
--- public/js/app.js
+++ public/js/app.js
@@ -5,22 +5,36 @@ const res = document.querySelector('.res')
 // Add a new request
 form.addEventListener('submit', async e => {
   e.preventDefault();
-  try{
+  let futureQuery;
+  const functions = firebase.app().functions(`us-central1`)
+  try {
     console.info(`submitted value:`, form.word.value, `and type: `, form.type.value)
     res.textContent = `loading`
 
-    const functions = firebase.app().functions(`us-central1`)
     const addRequest = functions.httpsCallable('getWordResult');
     const result = await addRequest({
       type: form.type.value,
       word: form.word.value,
     })
-    res.textContent = result.data.word ? JSON.stringify(result.data, undefined, 2) : result.data.toString()
-  }catch (e) {
+    if (result.data.word) {
+      futureQuery = result.data.futureQuery
+    }
+    res.textContent = result.data.word ? JSON.stringify(result.data.current, undefined, 2) : result.data.toString()
+  } catch (e) {
     console.log(e)
     res.textContent = e
     setTimeout(() => {
       res.textContent = ''
     }, 3000)
+  } finally {
+    const {when, encrypted, words} = futureQuery || {}
+    if(!!futureQuery && !!when && !!encrypted){
+      const fQuery = functions.httpsCallable('loopWords')
+      words.forEach(word => {
+        fQuery({
+          when, encrypted, word
+        })
+      })
+    }
   }
 });
