509d29f23406f428a6d0452a8a9aae6d8748efa5
Nasir <nasir.rubel99@gmail.com>
2021-08-21 20:52:42 +0600

    word trim on submit

--- public/js/app.js
+++ public/js/app.js
@@ -5,20 +5,19 @@ const res = document.querySelector('.res')
 // Add a new request
 form.addEventListener('submit', async e => {
   e.preventDefault();
+  const word = form.word.value.trim(),
+    type = form.type.value.trim()
+  console.log(word, word.length)
   let futureQuery;
   const functions = firebase.app().functions(`us-central1`)
   try {
-    console.info(`submitted value:`, form.word.value, `and type: `, form.type.value)
     res.textContent = `loading`
     for(const el of formEl.elements){
       el.setAttribute('disabled', 'disabled')
     }
 
     const addRequest = functions.httpsCallable('getWordResult');
-    const result = await addRequest({
-      type: form.type.value,
-      word: form.word.value,
-    })
+    const result = await addRequest({type, word})
     if (result.data.word) {
       futureQuery = result.data.futureQuery
     }
@@ -26,9 +25,7 @@ form.addEventListener('submit', async e => {
   } catch (e) {
     console.log(e)
     res.textContent = e
-    setTimeout(() => {
-      res.textContent = ''
-    }, 3000)
+    setTimeout(() => res.textContent = '', 3000)
   } finally {
     for(const el of formEl.elements){
       el.removeAttribute('disabled')
@@ -36,11 +33,7 @@ form.addEventListener('submit', async e => {
     const {when, encrypted, words} = futureQuery || {}
     if(!!futureQuery && !!when && !!encrypted){
       const fQuery = functions.httpsCallable('loopWords')
-      words.forEach(word => {
-        fQuery({
-          when, encrypted, word
-        })
-      })
+      words.forEach(word => fQuery({when, encrypted, word}))
     }
   }
 });
